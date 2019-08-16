6b14bc6c5dcda937277401d4e0178da159e56813
Oscar Nava Trujillo <oscarnava@gmail.com>
2019-08-16 16:56:27 -0500

    Solved linter issue: Method has too many lines

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,7 @@
 def bubble_sort!(list)
   last = list.size
   loop do
-    flag = false
-    current = 1
+    flag, current = false, 1
     while current < last
       if list[current] < list[current - 1]
         list[current], list[current - 1] = list[current - 1], list[current]
@@ -12,6 +11,5 @@ def bubble_sort!(list)
     end
     break if !flag
     last -= 1
-  end
-  list
+  end; list
 end
